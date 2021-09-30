d76d2efbe4b601a7fedffe08437e2c3b1a58924a
davegregg <davegregg@gmail.com>
2021-09-30 13:14:44 -0400

    Demo code

--- main.js
+++ main.js
@@ -19,39 +19,102 @@
   stopTimer(): stop the timer, then render()
 */
 
-const countView = document.querySelector("#count")
-let count = 0
+/*
+  Object-Oriented Programming:
+    - Objects & Methods: Bundle data and related functions together (encapsulation).
+    - Polymorphism: ...
 
-function render () {
-  countView.replaceChildren(count)
-  console.log(count)
+  "Method": a function which is a property on an object.
+  
+  "this": is the context in which the function was CALLED - not necessarily the context in which it was written.
+  
+  What is the "calling context"? It is (usually) whatever is to the left of the dot when you call the function: counter.next() - in this case, the counter object.
+*/
+
+const counter = {
+  count: 0,
+  countView: document.querySelector("#count"),
+  timerId: null,
+
+  render: function render () {
+    this.countView.replaceChildren(this.count)
+    console.log(this.count)
+  },
+
+  next: function () {
+    this.count += 1
+    this.render()
+  },
+
+  prev: function prev () {
+    this.count -= 1
+    this.render()
+  },
+  
+  reset: function reset () {
+    this.count = 0
+    this.render()
+  },
+  
+  skipForward: function skipForward () {
+    this.count += 10
+    this.render()
+  },
+  
+  skipBackward: function skipBackward () {
+    this.count -= 10
+    this.render()
+  },
+
+  startTimer: function startTimer () {
+    this.timerId = window.setInterval(() => this.next(), 1000)
+  },
+
+  stopTimer: function stopTimer () {
+    window.clearInterval(this.timerId)
+  },
 }
 
-function next () {
-  count += 1
-  render()
-}
 
-function prev () {
-  count -= 1
-  render()
-}
+const newsletterCounter = {
+  count: 500,
+  countView: document.querySelector("#newsletter-count"),
 
-function reset () {
-  count = 0
-  render()
-}
-
-function skipForward () {
-  count += 10
-  render()
-}
-
-function skipBackward () {
-  count -= 10
-  render()
+  render: counter.render,
+  next: counter.next,
+  prev: counter.prev,
+  reset: counter.reset,
+  skipForward: counter.skipForward,
+  skipBackward: counter.skipBackward,
 }
 
 
 
+document
+  .querySelector("#controls")
+  .addEventListener("click", event => {
+    const clickedElement = event.target
 
+    switch (clickedElement.id) {
+      case "next": counter.next()
+      break
+
+      case "prev": counter.prev()
+      break
+
+      case "reset": counter.reset()
+      break
+
+      case "skip-forward": counter.skipForward()
+      break
+
+      case "skip-backward": counter.skipBackward()
+      break
+
+      case "start-timer": counter.startTimer()
+      break
+
+      case "stop-timer": counter.stopTimer()
+      break
+    }
+  })
